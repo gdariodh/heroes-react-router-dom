2351c7703cc987c946aace2142e54c31c8b49b3a
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-16 00:57:08 -0400

    history -> push & replace

--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -1,7 +1,20 @@
-const LoginComponent = () => {
-    return ( 
-        <h3>Login component</h3>
-     );
-}
- 
+const LoginComponent = ({ history }) => {
+  const handleClick = () => {
+    // push & replace -> redirects spa
+    // history.push('/') -> no borra el historial 
+    history.replace("/"); // borra el historial, si le dan back a la url anterior no podran acceder
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button onClick={handleClick} className="btn btn-primary">
+        xd
+      </button>
+    </div>
+  );
+};
+
 export default LoginComponent;
