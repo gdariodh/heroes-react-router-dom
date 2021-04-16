f607564988a89ff9758ecd403bf256b44755081a
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-15 23:02:49 -0400

    add children routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,17 @@
 // AppRouter es el fichero main del routing del spa - este es que se manda al App.js
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginComponent from "../components/login/LoginComponent";
-// layout components
-import NavBar from "../components/ui/NavBar";
-// children components
-import MarvelComponent from "../components/marvel/MarvelComponent";
+import DashboardRoutes from "./DashboardRoutes";
 
+// routing main
 const AppRouter = () => {
   return (
     <Router>
-      <NavBar/>
-        <Switch>
-          <Route exact path="/" component={MarvelComponent} />
-          <Route exact path="/login" component={LoginComponent} />
-        </Switch>
+      <Switch>
+        <Route exact path="/login" component={LoginComponent} />
+        {/* routing children */}
+        <Route path="/" component={DashboardRoutes} />
+      </Switch>
     </Router>
   );
 };
