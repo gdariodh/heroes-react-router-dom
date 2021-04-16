a66c48ae9ff8dac6e42b01c3ce360f2866ec3a8e
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-15 23:02:35 -0400

    initializing children routes

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,26 @@
+// routing children
+import { Redirect, Route, Switch } from "react-router";
+import DcComponent from "../components/dc/DcComponent";
+import HeroesComponent from "../components/heroes/HeroesComponent";
+import LoginComponent from "../components/login/LoginComponent";
+import MarvelComponent from "../components/marvel/MarvelComponent";
+import Navbar from "../components/ui/NavBar";
+
+const DashboardRoutes = () => {
+  return (
+    <>
+      {/* layout */}
+      <Navbar />
+      {/* routing */}
+      <Switch>
+        <Route exact path="/marvel" component={MarvelComponent} />
+        <Route exact path="/marvel:heroeId" component={HeroesComponent} />
+        <Route exact path="/dc" component={DcComponent} />
+        {/* Si no es ninguna de la anteriores rutas, redirect a marvel por defecto  */}
+        <Redirect to="/marvel" />
+      </Switch>
+    </>
+  );
+};
+
+export default DashboardRoutes;
