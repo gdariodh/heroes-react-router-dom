2595fcc780d1d4d8fbc68df8485cd85b28462a40
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-17 13:28:38 -0400

    agregando useMemo

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,11 +1,20 @@
 // filters
+import { useMemo } from "react";
 import getHeroesByPublisher from "../../selectors/getHeroesByPublisher";
 import HeroeCard from "./HeroeCard";
 // components
 
 // template component of heroes like Marvel or DC
 const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+
+  // useMemo se usa para optimizar perfomance, lo que hace es que si no cambia el publisher
+  // no vuelve a disparar el filter getHeroesByPublisher, y solamente tiene que renderizar la info
+  // que ya tiene, la cual esta memorizada ya que no ha cambiado. 
+  // Pero si cambia publisher lanza la el filter
+
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
+
+  // option comun, sin useMemo -> const heroes = getHeroesByPublisher(publisher);
 
   return (
     <div className="card-columns">
