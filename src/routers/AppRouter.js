4d22d291d21dad49aaa12ec864561e4074594f89
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-04-25 19:56:34 -0400

    adding rutas protegidas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,24 @@
 // AppRouter es el fichero main del routing del spa - este es que se manda al App.js
+import { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginComponent from "../components/login/LoginComponent";
 import DashboardRoutes from "./DashboardRoutes";
+// retorna rutas privadas si esta auth
+import PrivateRoute from "./PrivateRoute";
+import AuthContext from "../auth/authContext";
 
 // routing main
 const AppRouter = () => {
+  // acceder al state de user
+  const { user } = useContext(AuthContext);
+  const isAuth = user.logged;
+
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={LoginComponent} />
         {/* routing children */}
-        <Route path="/" component={DashboardRoutes} />
+        <PrivateRoute path="/" isAuth={isAuth} component={DashboardRoutes} />
       </Switch>
     </Router>
   );
